21fd2f2f22f0b45e1c2e24454383c63a890dd9dd
Tom Fan <tomfancode@gmail.com>
2026-01-09 00:07:35 +0800

    fix: remove sitemap integration from astro.config.mjs

--- my-app/astro.config.mjs
+++ my-app/astro.config.mjs
@@ -1,11 +1,11 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://leetcode.tfix.io",
-  integrations: [mdx(), react(), tailwind(), sitemap()],
+  integrations: [mdx(), react(), tailwind()],
 });

74eb0a298149b9a71dbc64c1b7b025846909084e
Tom Fan <tomfancode@gmail.com>
2026-01-09 00:01:07 +0800

    fix: reorder imports in astro.config.mjs

--- my-app/astro.config.mjs
+++ my-app/astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://leetcode.tfix.io",
-  integrations: [mdx(), react(), sitemap(), tailwind()],
+  integrations: [mdx(), react(), tailwind(), sitemap()],
 });
